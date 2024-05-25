164d02a0292e870771f316b21a6b2160f518a2c6
niyathi18 <niyathisurthineni1801@gmail.com>
2024-05-25 17:10:30 +0530

    add card component

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,22 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//     images: {
+//         domains: ['image.tmdb.org',]
+//     },
+// };
 
-export default nextConfig;
+// export default nextConfig;
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 'image.tmdb.org',
+          pathname: '**',
+        },
+      ],
+    },
+  };
+  export default nextConfig;
