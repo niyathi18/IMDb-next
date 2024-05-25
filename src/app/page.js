930ac992b92099a3e7dd8c44b212570912123d4a
niyathi18 <niyathisurthineni1801@gmail.com>
2024-05-25 16:37:59 +0530

    add loading effect using NEXT.js

--- src/app/page.js
+++ src/app/page.js
@@ -29,7 +29,8 @@ export default async function Home({ searchParams }) {
     const res = await fetch(
       `https://api.themoviedb.org/3${
         genre === 'fetchTopRated' ? '/movie/top_rated' : '/trending/all/week'
-      }?api_key=${API_KEY}&language=en-US&page=1`
+      }?api_key=${API_KEY}&language=en-US&page=1`,
+      {next:{revalidate:10000}}
     );
 
     if (!res.ok) {
